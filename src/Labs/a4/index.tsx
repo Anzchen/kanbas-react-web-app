bf36cea8a548a5bbe67644ac2e3f176f00f756f9
Anzchen <Anthonyzchen@yahoo.com>
2024-03-04 22:59:58 -0500

    Lab Finished 2.5.2 maybe some typo?

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,9 +1,49 @@
-function Assignment4() {
- return (
-   <div className="container">
-     <h1>Assignment 4</h1>
-   </div>
- );
-}
+import React from "react";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
+import TodoList from "./ReduxExamples/todos/TodoList";
+import { useSelector } from "react-redux";
+import { LabState } from "../store";
 
+function Assignment4() {
+  const { todos } = useSelector((state: LabState) => state.todosReducer);
+  function sayHello() {
+    alert("Hello");
+  }
+  return (
+    <div>
+      <h1>Assignment 4</h1>
+      <ClickEvent/>
+      <PassingDataOnEvent/>
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject/>
+      <Counter/>
+      <BooleanStateVariables/>
+      <StringStateVariables/>
+      <DateStateVariable/>
+      <ObjectStateVariable/>
+      <ArrayStateVariable/>
+      <ParentStateComponent/>
+      <ReduxExamples/>
+      <TodoList/>
+      <ul className="list-group">
+        {todos.map((todo) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 export default Assignment4;
