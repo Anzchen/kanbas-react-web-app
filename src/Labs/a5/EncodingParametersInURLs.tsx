38c25fd84995d8e5b17f797e2cb3aa806e1e9d8c
Anzchen <Anthonyzchen@yahoo.com>
2024-03-26 17:36:36 -0400

    api_base

--- src/Labs/a5/EncodingParametersInURLs.tsx
+++ src/Labs/a5/EncodingParametersInURLs.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+const API_BASE = process.env.REACT_APP_API_BASE;
 function EncodingParametersInURLs() {
   const [a, setA] = useState(34);
   const [b, setB] = useState(23);
   const [welcome, setWelcome] = useState("");
   const fetchWelcome = async () => {
-    const response = await axios.get("http://localhost:4000/a5/welcome");
+    const response = await axios.get(`${API_BASE}/a5/welcome`);
     setWelcome(response.data);
   };
   useEffect(() => {
@@ -14,12 +15,12 @@ function EncodingParametersInURLs() {
   const [result, setResult] = useState(0);
   const fetchSum = async (a: number, b: number) => {
     const response = await
-      axios.get(`http://localhost:4000/a5/add/${a}/${b}`);
+      axios.get(`${API_BASE}/a5/add/${a}/${b}`);
     setResult(response.data);
   };
   const fetchSubtraction = async (a: number, b: number) => {
     const response = await axios.get(
-      `http://localhost:4000/a5/subtract/${a}/${b}`);
+      `${API_BASE}/a5/subtract/${a}/${b}`);
     setResult(response.data);
   };
 
@@ -43,28 +44,28 @@ function EncodingParametersInURLs() {
         Fetch Substraction of {a} - {b}
       </button>
       <h3>Path Parameters</h3>
-      <a href={`http://localhost:4000/a5/add/${a}/${b}`}>
+      <a href={`${API_BASE}/a5/add/${a}/${b}`}>
         Add {a} + {b}
       </a>
       <br></br>
-      <a href={`http://localhost:4000/a5/subtract/${a}/${b}`}>
+      <a href={`${API_BASE}/a5/subtract/${a}/${b}`}>
         Substract {a} - {b}
       </a>
       <h3>Query Parameters</h3>
       <a className="btn btn-primary"
-      href={`http://localhost:4000/a5/calculator?operation=add&a=${a}&b=${b}`}>
+      href={`${API_BASE}/a5/calculator?operation=add&a=${a}&b=${b}`}>
         Add {a} + {b}
       </a>
       <a className="btn btn-danger"
-      href={`http://localhost:4000/a5/calculator?operation=subtract&a=${a}&b=${b}`}>
+      href={`${API_BASE}/a5/calculator?operation=subtract&a=${a}&b=${b}`}>
         Substract {a} - {b}
       </a>
       <a className="btn btn-primary"
-      href={`http://localhost:4000/a5/calculator?operation=multiply&a=${a}&b=${b}`}>
+      href={`${API_BASE}/a5/calculator?operation=multiply&a=${a}&b=${b}`}>
         Multiply {a} - {b}
       </a>
       <a className="btn btn-danger"
-      href={`http://localhost:4000/a5/calculator?operation=divide&a=${a}&b=${b}`}>
+      href={`${API_BASE}/a5/calculator?operation=divide&a=${a}&b=${b}`}>
         Divide {a} - {b}
       </a>
     </div>
--- src/Labs/a5/WorkingWithObjects.tsx
+++ src/Labs/a5/WorkingWithObjects.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+const API_BASE = process.env.REACT_APP_API_BASE;
 function WorkingWithObjects() {
     const [assignment, setAssignment] = useState({
         id: 1, title: "NodeJS Assignment",
         description: "Create a NodeJS server with ExpressJS",
         due: "2021-10-10", completed: false, score: 0,
       });
-    const ASSIGNMENT_URL = "http://localhost:4000/a5/assignment"
+    const ASSIGNMENT_URL = `${API_BASE}/a5/assignment`
     const fetchAssignment = async () => {
         const response = await axios.get(`${ASSIGNMENT_URL}`);
         setAssignment(response.data);
@@ -43,11 +44,11 @@ function WorkingWithObjects() {
             title: e.target.value })}
         value={assignment.title}/>
       <h4>Retrieving Objects</h4>
-      <a href="http://localhost:4000/a5/assignment">
+      <a href={`${API_BASE}/a5/assignment`}>
         Get Assignment
       </a>
       <h4>Retrieving Properties</h4>
-      <a href="http://localhost:4000/a5/assignment/title">
+      <a href={`${API_BASE}/a5/assignment/title`}>
         Get Title
       </a>
     </div>
